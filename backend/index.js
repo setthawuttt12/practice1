0570d0c4bf31b8b69d3ac2c00091bcb9087be647
yootoshima <yootoshi53258@gmail.com>
2026-07-02 17:51:43 +0700

    showeva success

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,9 @@ app.use('/api/docnoe',docnoe)
 const show_eva = require('./routes/Commit/show_eva')
 app.use('/api/Commit/show_eva',show_eva)
 
+const score_member2 = require('./routes/Commit/score_member')
+app.use('/api/Commit/score_member',score_member2)
+
 ///Eva
 const edit_eva = require('./routes/Eva/edit_eva')
 app.use('/api/Eva/edit_eva',edit_eva)
